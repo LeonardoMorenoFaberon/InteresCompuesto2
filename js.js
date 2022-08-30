225e8e1be606498ed16997f3121f93b5d250a64b
lemofa <lemofa777@gmail.com>
2022-08-30 18:03:59 -0500

    cambios en js

--- js.js
+++ js.js
@@ -62,8 +62,56 @@ function trio(valor , n){
 
 //......................................
 function showIncalculatorScreens(){
-    idcalculatorScreen1.innerText = Intl.NumberFormat('de-DE', { style: 'currency', currency: 'SOL' }).format(valorAcumulado);;
-    idcalculatorScreen2.innerText = Intl.NumberFormat('de-DE', { style: 'currency', currency: 'SOL' }).format(intSimple(monto , interes , veces));
+    idcalculatorScreen1.innerText = Intl.NumberFormat('de-DE', { style: 'currency', currency: 'DOL' }).format(valorAcumulado);;
+    idcalculatorScreen2.innerText = Intl.NumberFormat('de-DE', { style: 'currency', currency: 'DOL' }).format(intSimple(monto , interes , veces));
     shower.classList.remove('oculto');
 }
+
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+
+
+
+// let x1 = 10;
+let x2 = .3;
+let xIncrement =  .3;
+// let y1 =  function(x){ return 1*Math.pow(1.02,x)};
+let y1 =  function(x , ciclos){ return  1-150/(1*Math.pow(1.02 , x))    };
+let y2 =  150;
+let ciclos =  900;
+
+for(let x1=0; x1 <= ciclos ; x1+= 300/ciclos){
+    // ctx.fillRect(x1 ,(1- 150/y1(x1, ciclos))  , ciclos ,150);
+    ctx.fillRect(x1 , (y1(x1, ciclos))  , ciclos ,150);
+    // ctx.fillRect(x1 ,   , ciclos ,150);
+}
+
+
+
+// necesito que el eje Y para el maximo valor de x1 tome 150px
+// 1.02 ^ 253 = 149.91
+// para x1=300 x = 253  esto puede ser una funcion lineal.
+// donde x es el valor de la funcion que se repite en el ciclo for
+// por lo tanto el valor de x no tendra por q ser igual al valor de x1
+
+
+
+
+// ctx.fillRect(20, (150 - y1(20)), 21, (150 - 0));
+// ctx.fillRect(25, (150 - y1(25)), 30, (150 - 0));
+
+// ctx.fillRect(10, 10, 100 , 100);
+// ctx.fillRect(30, 30, 100 , 100);
+
+
+
+
+
+
+
+
+
 // console.log(    intCompuesto(100 , 3 , 120)*5/100/12    )
